5788476c9754308673a3606069a73f5fd9270109
Mykyta Zaitsev <mykyta.zaitsev@student.pk.edu.pl>
2026-06-19 14:55:32 +0200

    Separate driver model from program

--- Program.cs
+++ Program.cs
@@ -2,38 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class Driver
-{
-    public string Name { get; }
-    public List<double> LapTimes { get; }
-
-    public Driver(string name)
-    {
-        Name = name;
-        LapTimes = new List<double>();
-    }
-
-    public void AddLapTime(double lapTime)
-    {
-        if (lapTime <= 0)
-        {
-            throw new ArgumentException("Lap time must be greater than zero.");
-        }
-
-        LapTimes.Add(lapTime);
-    }
-
-    public double GetBestLapTime()
-    {
-        return LapTimes.Count == 0 ? 0 : LapTimes.Min();
-    }
-
-    public double GetAverageLapTime()
-    {
-        return LapTimes.Count == 0 ? 0 : LapTimes.Average();
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
