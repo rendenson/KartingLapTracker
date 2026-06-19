b0d2b927b523a3231a41f9d2e0366cf1f8626fd5
Mykyta Zaitsev <mykyta.zaitsev@student.pk.edu.pl>
2026-06-19 23:43:36 +0200

    Add interactive console menu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
@@ -8,30 +7,75 @@ class Program
     {
         List<Driver> drivers = new List<Driver>();
 
-        Driver oliver = new Driver("Oliver");
-        oliver.AddLapTime(52.249);
-        oliver.AddLapTime(51.977);
-        oliver.AddLapTime(52.501);
-        drivers.Add(oliver);
-
-        Driver james = new Driver("James");
-        james.AddLapTime(53.865);
-        james.AddLapTime(52.903);
-        james.AddLapTime(52.239);
-        drivers.Add(james);
-
-        Driver henry = new Driver("Henry");
-        henry.AddLapTime(54.139);
-        henry.AddLapTime(52.349);
-        henry.AddLapTime(51.023);
-        drivers.Add(henry);
-
-        foreach (Driver driver in drivers)
+        while (true)
         {
-            Console.WriteLine($"Driver: {driver.Name}");
-            Console.WriteLine($"Best lap: {driver.GetBestLapTime():F3} seconds");
-            Console.WriteLine($"Average lap: {driver.GetAverageLapTime():F3} seconds");
-            Console.WriteLine();
+            Console.WriteLine("\n= Karting Lap Tracker = ");
+            Console.WriteLine("1. Add driver");
+            Console.WriteLine("2. Show all drivers");
+            Console.WriteLine("3. Exit");
+            Console.Write("Choose an option: "); 
+
+            string? input = Console.ReadLine();
+
+            // Validation cycle
+            while(input != "1" && input != "2" && input != "3")
+            {
+                Console.Write("Unknown option, please try again: ");
+                input = Console.ReadLine();
+            }
+
+            switch (input)
+            {
+                case "1":
+                    Console.Write("Name a driver: ");
+                    string? inputName = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(inputName))
+                    {
+                        Console.WriteLine("Name cannot be empty.");
+                        break;
+                    }
+
+                    Driver newDriver = new Driver(inputName);
+
+                    drivers.Add(newDriver);
+                    Console.WriteLine($"Driver '{inputName}' added.");
+                    break;
+
+                case "2":
+                    Console.WriteLine("The list of all drivers:");
+                    
+                    if (drivers.Count == 0)
+                    {
+                        Console.WriteLine("No drivers yet...");
+                    }
+
+                    foreach(Driver driver in drivers)
+                    {
+                        Console.WriteLine($"\nDriver: {driver.Name}");  
+                        
+                        if (driver.LapTimes.Count != 0)
+                        {
+                            double? best = driver.GetBestLapTime();
+                            Console.WriteLine($"Best lap: {best:F3} seconds");
+
+                            double? avg = driver.GetAverageLapTime();
+                            Console.WriteLine($"Average lap: {avg:F3} seconds");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No lap times yet...");
+                        }
+                    }
+
+                    Console.WriteLine("\nPress enter to continue...");
+                    Console.ReadLine();
+                    break;
+
+                case "3":
+                    Console.WriteLine("Successfully exited.");
+                    return;
+            }
         }
     }
 }
