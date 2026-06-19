159b4ff060f045363feda20756ef2d552c00a458
Mykyta Zaitsev <mykyta.zaitsev@student.pk.edu.pl>
2026-06-19 13:53:41 +0200

    Improve code formatting

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ using System.Linq;
 
 class Driver
 {
-    public string Name { get;}
-    public List<double> LapTimes { get;}
+    public string Name { get; }
+    public List<double> LapTimes { get; }
 
     public Driver(string name)
     {
@@ -36,35 +36,34 @@ class Driver
 
 class Program
 {
-     static void Main(string[] args){
-            List<Driver> drivers = new List<Driver>();
+    static void Main(string[] args)
+    {
+        List<Driver> drivers = new List<Driver>();
 
-            Driver oliver = new Driver("Oliver");
-            oliver.AddLapTime(52.249);
-            oliver.AddLapTime(51.977);
-            oliver.AddLapTime(52.501);
-            drivers.Add(oliver);
+        Driver oliver = new Driver("Oliver");
+        oliver.AddLapTime(52.249);
+        oliver.AddLapTime(51.977);
+        oliver.AddLapTime(52.501);
+        drivers.Add(oliver);
 
-            Driver james = new Driver("James");
-            james.AddLapTime(53.865);
-            james.AddLapTime(52.903);
-            james.AddLapTime(52.239);
-            drivers.Add(james);
+        Driver james = new Driver("James");
+        james.AddLapTime(53.865);
+        james.AddLapTime(52.903);
+        james.AddLapTime(52.239);
+        drivers.Add(james);
 
-            Driver henry = new Driver("Henry");
-            henry.AddLapTime(54.139);
-            henry.AddLapTime(52.349);
-            henry.AddLapTime(51.023);
-            drivers.Add(henry); 
+        Driver henry = new Driver("Henry");
+        henry.AddLapTime(54.139);
+        henry.AddLapTime(52.349);
+        henry.AddLapTime(51.023);
+        drivers.Add(henry);
 
-            foreach (Driver driver in drivers)
-            {
-                Console.WriteLine($"Driver: {driver.Name}");
-                Console.WriteLine($"Best lap: {driver.GetBestLapTime():F3}");
-                Console.WriteLine($"Average lap: {driver.GetAverageLapTime():F3}");
-                Console.WriteLine();
-            }
-
+        foreach (Driver driver in drivers)
+        {
+            Console.WriteLine($"Driver: {driver.Name}");
+            Console.WriteLine($"Best lap: {driver.GetBestLapTime():F3} seconds");
+            Console.WriteLine($"Average lap: {driver.GetAverageLapTime():F3} seconds");
+            Console.WriteLine();
+        }
     }
 }
-
